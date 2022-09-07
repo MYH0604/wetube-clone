2f5b253bfc602d654ff0e055f99cc64f20b56c72
MYH0604 <octocb97@naver.com>
2022-09-08 03:11:53 +0900

    Update Clone - Pug, POST, etc

--- README.md
+++ README.md
@@ -5,11 +5,12 @@
 /login -> Login
 /serach -> Search
 
-/users/edit -> Edit user
-/users/delete -> Delete user
+/users/:id -> See user
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile
 
-/videos/watch -> Watch Video
-/videos/edit -> Edit Video
-/videos/delete-> Delete Video
-/videos/comments -> Comment on a Video
-/videos/comments/delete -> Delete a Comment of a Video
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete-> Delete Video
+/videos/upload -> Upload Video
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,68 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch Video");
-export const edit = (req, res) => res.send("Edit Video");
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: true,
+};
+
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 0,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+];
+
+export const trending = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id];
+  res.render(`watch`, { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id];
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const newVideo = {
+    title: req.body.title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
