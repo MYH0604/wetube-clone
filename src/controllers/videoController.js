bc25bf8bc6735e39e4662f843d7078d91a68bae1
MYH0604 <octocb97@naver.com>
2022-10-09 20:34:56 +0900

    update aws

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -62,8 +62,8 @@ export const postUpload = async (req, res) => {
     const newVideo = await Video.create({
       title,
       description,
-      thumbUrl: thumb[0].path,
-      fileUrl: video[0].path,
+      thumbUrl: thumb[0].location,
+      fileUrl: video[0].location,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
