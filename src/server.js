5c81301f6a33dbeab2c11cec64f047405bc39a87
MYH0604 <octocb97@naver.com>
2022-10-09 22:16:15 +0900

    bug fix

--- src/server.js
+++ src/server.js
@@ -39,8 +39,6 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
 app.use("/videos", videoRouter);
